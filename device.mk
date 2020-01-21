83a599761e932a730534ecef2ad974b3e0cc0764
OdSazib <odsazib@gmail.com>
2020-01-21 14:37:22 +0530

    whyred: Add powerhint.json for sdm636
    Co-authored-by: Subhajeet Muhuri <kenny3fcb@gmail.com>
    Co-authored-by: clarencelol <clarencekuiek@icloud.com>
    Change-Id: I3591038a29326b15ce00de0051208219d9f3830d
    Signed-off-by: TheSanty <sudhiryadav.igi@gmail.com>

--- device.mk
+++ device.mk
@@ -47,6 +47,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
     $(DEVICE_PATH)/overlay-lineage
 
+# Powerhint
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
