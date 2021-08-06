ff3edeb2ffc549e4e306e4e1886e53245a5199f6
TheSanty <sudhiryadav.igi@gmail.com>
2021-08-06 11:31:29 +0530

    whyred: Corvus-ify
    Signed-off-by: TheSanty <sudhiryadav.igi@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_whyred.mk
+    $(LOCAL_DIR)/corvus_whyred.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_whyred-user \
-    aosp_whyred-userdebug \
-    aosp_whyred-eng
+    corvus_whyred-user \
+    corvus_whyred-userdebug \
+    corvus_whyred-eng
--- corvus_whyred.mk
+++ corvus_whyred.mk
@@ -19,16 +19,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common Hycon stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Corvus stuff
+$(call inherit-product, vendor/corvus/config/common_full_phone.mk)
 
-TARGET_GAPPS_ARCH := arm64
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_USES_FACE_UNLOCK := true
-
-# HyconOS Official
-HYCON_BUILD_TYPE := OFFICIAL
-HYCON_MAINTAINER := TheSanty
+# Corvus Maintainer
+CORVUS_MAINTAINER := TheSanty
 
 # Inherit from whyred device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -36,7 +31,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := aosp_whyred
+PRODUCT_NAME := corvus_whyred
 PRODUCT_MODEL := Redmi Note 5
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
