a780f08842b7bf2b451f459ad09cf4aa8da98309
TheSanty <sudhiryadav.igi@gmail.com>
2021-09-03 22:57:46 +0530

    whyred: Bringup HyconOS
    Signed-off-by: TheSanty <sudhiryadav.igi@gmail.com>
    Change-Id: I42f0c2ce64e261d13dee73c162ac0bbe269b0dea

--- aosp_whyred.mk
+++ aosp_whyred.mk
@@ -19,8 +19,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Hycon stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+TARGET_GAPPS_ARCH := arm64
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_USES_FACE_UNLOCK := true
+
+# HyconOS Official
+HYCON_BUILD_TYPE := OFFICIAL
+HYCON_MAINTAINER := TheSanty
 
 # Inherit from whyred device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -28,7 +36,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_whyred
+PRODUCT_NAME := aosp_whyred
 PRODUCT_MODEL := Redmi Note 5
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
