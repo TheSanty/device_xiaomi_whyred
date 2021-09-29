2b248e7e309e71925ebd94b15a6eeff68eb838f0
TheSanty <sudhiryadav.igi@gmail.com>
2021-09-30 00:05:25 +0530

    whyred: Initialize Evolution X 5.x
    Signed-off-by: TheSanty <sudhiryadav.igi@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_whyred.mk
+    $(LOCAL_DIR)/evolution_whyred.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_whyred-user \
-    aosp_whyred-userdebug \
-    aosp_whyred-eng
+    evolution_whyred-user \
+    evolution_whyred-userdebug \
+    evolution_whyred-eng
--- evolution_whyred.mk
+++ evolution_whyred.mk
@@ -9,16 +9,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common Hycon stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common evolution stuff
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 TARGET_GAPPS_ARCH := arm64
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_USES_FACE_UNLOCK := true
-
-# HyconOS Official
-HYCON_BUILD_TYPE := OFFICIAL
-HYCON_MAINTAINER := TheSanty
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit from whyred device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -29,7 +26,7 @@ $(call inherit-product, vendor/MiuiCamera/config.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := aosp_whyred
+PRODUCT_NAME := evolution_whyred
 PRODUCT_MODEL := Redmi Note 5
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
